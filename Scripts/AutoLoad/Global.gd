aacfb0f97646386c5def6d1f6b581d244706b433
gruebite <gruebite@gmail.com>
2023-02-04 09:11:50 -0800

    Adjust layout, add card spots.

--- Scripts/AutoLoad/Global.gd
+++ Scripts/AutoLoad/Global.gd
@@ -1,6 +1,6 @@
 extends Node
 
-
+# Offset square layout.
 var screen_offset_hex_layout := HexLayout.new(Vector3(63, 49, 0), Vector3.ZERO)
 
 # Layout for display.  Used for translating between screen coordinates and hex coordinates.
@@ -9,6 +9,9 @@ var screen_offset_hex_layout := HexLayout.new(Vector3(63, 49, 0), Vector3.ZERO)
 # effectively each hexagon is 63x49 (offset squares), and for pixel perfect translation
 # hexagons are: 63/sqrt(3) by size/2 or:
 var pixel_hex_layout := HexLayout.new(Vector3(36.373066958946424, 32, 0), Vector3(32, 32, 0))
+
+# Playing board size.
+var board_hex_set := HexSets.rectangle(0, 11, 0, 11)
 
 # Global RNG.
 var rand := Random.new()
