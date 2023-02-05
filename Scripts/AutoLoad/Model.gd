7b091f7b20e22834dcaea9f21506002b9e1ec762
gruebite <gruebite@gmail.com>
2023-02-05 01:08:40 -0800

    Add plant variety.

--- Scripts/AutoLoad/Model.gd
+++ Scripts/AutoLoad/Model.gd
@@ -1,17 +1,25 @@
 class_name Model extends Node
 
-
 enum Plant {
 	WATER,
 	YGGDRASIL,
 	MIGHTY_OAK,
 	CONY_FIERCE,
+	ROCK_SUCCULENT,
+	GRASS,
 	BALL_CAP,
+	SNOOZE_BERRIES,
 	SUN_FLOWER,
 	CATS_TAIL,
 	RABBITS_FOOT,
 	SQUIRREL_CORN,
-	FRON_FERN,
+	FROG_FERN,
+	BIRD_OF_PARADISE,
+	BEARS_PAW,
+	WOLFS_BANE,
+	BEE_ORCHID,
+	HIVE_SUCCULENT,
+	SPIDER_PLANT,
 }
 
 var plants := [
@@ -20,60 +28,133 @@ var plants := [
 		"texture": preload("res://Assets/Textures/Water.png"),
 		"likes": {},
 		"dislikes": {},
+		"weight": 0,
 	},
 	{
 		"name": "Yggdrasil",
 		"texture": preload("res://Assets/Textures/WorldTree.png"),
 		"likes": {},
 		"dislikes": {},
+		"weight": 0,
 	},
 	{
 		"name": "Mighty Oak",
 		"texture": preload("res://Assets/Textures/MightyOak.png"),
 		"likes": {},
 		"dislikes": {Plant.MIGHTY_OAK: true, Plant.CONY_FIERCE: true, Plant.SQUIRREL_CORN: true},
+		"weight": 10,
 	},
 	{
 		"name": "Cony Fierce",
-		"texture": preload("res://Assets/Textures/CatsTail.png"),
-		"likes": {Plant.WATER: true},
-		"dislikes": {Plant.BALL_CAP: true},
+		"texture": preload("res://Assets/Textures/ConyFierce.png"),
+		"likes": {Plant.ROCK_SUCCULENT: true, Plant.BALL_CAP: true, Plant.CONY_FIERCE: true},
+		"dislikes": {Plant.MIGHTY_OAK: true},
+		"weight": 10,
+	},
+	{
+		"name": "Rock Succulent",
+		"texture": preload("res://Assets/Textures/Roots.png"),
+		"likes": {Plant.ROCK_SUCCULENT: true},
+		"dislikes": {Plant.WATER: true},
+		"weight": 2,
+	},
+	{
+		"name": "Grass",
+		"texture": preload("res://Assets/Textures/Grass.png"),
+		"likes": {Plant.BEE_ORCHID: true},
+		"dislikes": {Plant.MIGHTY_OAK: true, Plant.CONY_FIERCE: true, Plant.ROCK_SUCCULENT: true},
+		"weight": 4,
 	},
 	{
 		"name": "Ball Cap",
-		"texture": preload("res://Assets/Textures/CatsTail.png"),
-		"likes": {Plant.MIGHTY_OAK: true, Plant.CONY_FIERCE: true},
-		"dislikes": {Plant.MIGHTY_OAK: true},
+		"texture": preload("res://Assets/Textures/BallCap.png"),
+		"likes": {Plant.MIGHTY_OAK: true, Plant.CONY_FIERCE: true, Plant.WATER: true},
+		"dislikes": {Plant.RABBITS_FOOT: true, Plant.SQUIRREL_CORN: true, Plant.BIRD_OF_PARADISE: true},
+		"weight": 3,
+	},
+	{
+		"name": "Snooze Berries",
+		"texture": preload("res://Assets/Textures/Roots.png"),
+		"likes": {Plant.MIGHTY_OAK: true, Plant.CONY_FIERCE: true, Plant.GRASS: true},
+		"dislikes": {Plant.WATER: true},
+		"weight": 5,
 	},
 	{
 		"name": "Sun Flower",
-		"texture": preload("res://Assets/Textures/CatsTail.png"),
-		"likes": {Plant.WATER: true},
-		"dislikes": {Plant.BALL_CAP: true},
+		"texture": preload("res://Assets/Textures/SunFlower.png"),
+		"likes": {Plant.BEE_ORCHID: true},
+		"dislikes": {Plant.RABBITS_FOOT: true},
+		"weight": 3,
 	},
 	{
 		"name": "Cat's Tail",
 		"texture": preload("res://Assets/Textures/CatsTail.png"),
 		"likes": {Plant.WATER: true},
 		"dislikes": {Plant.BALL_CAP: true},
+		"weight": 5,
 	},
 	{
 		"name": "Rabbit's Foot",
-		"texture": preload("res://Assets/Textures/CatsTail.png"),
-		"likes": {Plant.WATER: true},
-		"dislikes": {Plant.BALL_CAP: true},
+		"texture": preload("res://Assets/Textures/Roots.png"),
+		"likes": {Plant.BIRD_OF_PARADISE: true, Plant.SUN_FLOWER: true, Plant.GRASS: true},
+		"dislikes": {Plant.WOLFS_BANE: true, Plant.BIRD_OF_PARADISE: true},
+		"weight": 3,
 	},
 	{
 		"name": "Squirrel Corn",
-		"texture": preload("res://Assets/Textures/CatsTail.png"),
-		"likes": {Plant.WATER: true},
-		"dislikes": {Plant.BALL_CAP: true},
+		"texture": preload("res://Assets/Textures/SquirrelCorn.png"),
+		"likes": {Plant.MIGHTY_OAK: true, Plant.CONY_FIERCE: true},
+		"dislikes": {Plant.WOLFS_BANE: true, Plant.BIRD_OF_PARADISE: true},
+		"weight": 4,
 	},
 	{
 		"name": "Frog Fern",
-		"texture": preload("res://Assets/Textures/CatsTail.png"),
-		"likes": {Plant.WATER: true},
-		"dislikes": {Plant.BALL_CAP: true},
+		"texture": preload("res://Assets/Textures/FrogFern.png"),
+		"likes": {Plant.WATER: true, Plant.BEE_ORCHID: true, Plant.SPIDER_PLANT: true, Plant.ROCK_SUCCULENT: true},
+		"dislikes": {Plant.WOLFS_BANE: true, Plant.BIRD_OF_PARADISE: true},
+		"weight": 4,
+	},
+	{
+		"name": "Bird Of Paradise",
+		"texture": preload("res://Assets/Textures/BirdOfParadise.png"),
+		"likes": {Plant.MIGHTY_OAK: true, Plant.CONY_FIERCE: true, Plant.BIRD_OF_PARADISE: true, Plant.SUN_FLOWER: true, Plant.BEE_ORCHID: true},
+		"dislikes": {Plant.CATS_TAIL: true},
+		"weight": 2,
+	},
+	{
+		"name": "Bear's Paw",
+		"texture": preload("res://Assets/Textures/BearsPaw.png"),
+		"likes": {Plant.HIVE_SUCCULENT: true, Plant.RABBITS_FOOT: true, Plant.SNOOZE_BERRIES: true},
+		"dislikes": {Plant.BEE_ORCHID: true, Plant.WOLFS_BANE: true},
+		"weight": 2,
+	},
+	{
+		"name": "Wolf's Bane",
+		"texture": preload("res://Assets/Textures/Roots.png"),
+		"likes": {Plant.CATS_TAIL: true, Plant.SQUIRREL_CORN: true, Plant.FROG_FERN: true},
+		"dislikes": {Plant.WATER: true, Plant.MIGHTY_OAK: true, Plant.CONY_FIERCE: true, Plant.BEE_ORCHID: true, Plant.BEARS_PAW: true},
+		"weight": 1,
+	},
+	{
+		"name": "Bee Orchid",
+		"texture": preload("res://Assets/Textures/BeeOrchid.png"),
+		"likes": {Plant.HIVE_SUCCULENT: true, Plant.BEE_ORCHID: true, Plant.SUN_FLOWER: true, Plant.BIRD_OF_PARADISE: true},
+		"dislikes": {Plant.BEARS_PAW: true},
+		"weight": 3,
+	},
+	{
+		"name": "Hive Succulent",
+		"texture": preload("res://Assets/Textures/HiveSucculent.png"),
+		"likes": {Plant.MIGHTY_OAK: true, Plant.CONY_FIERCE: true},
+		"dislikes": {Plant.WATER: true, Plant.BEARS_PAW: true, Plant.SPIDER_PLANT: true},
+		"weight": 1,
+	},
+	{
+		"name": "Spider Plant",
+		"texture": preload("res://Assets/Textures/SpiderPlant.png"),
+		"likes": {Plant.MIGHTY_OAK: true, Plant.CONY_FIERCE: true, Plant.BEE_ORCHID: true},
+		"dislikes": {Plant.WATER: true, Plant.GRASS: true, Plant.SPIDER_PLANT: true},
+		"weight": 2,
 	},
 ]
 
@@ -109,13 +190,17 @@ var plant_cards := []
 var root_cards := []
 
 func _ready() -> void:
-	for i in range(60):
-		if i < 20:
-			plant_cards.append(PlantCard.new(Plant.MIGHTY_OAK))
-		elif i < 40:
-			plant_cards.append(PlantCard.new(Plant.CATS_TAIL))
-		else:
-			plant_cards.append(PlantCard.new(Plant.BALL_CAP))
+	var total_plant_weight := 0
+	for def in plants:
+		total_plant_weight += def.weight
+
+	for _i in range(60):
+		var r := GLOBAL.rand.rangei(0, total_plant_weight)
+		for i in range(plants.size()):
+			r -= plants[i].weight
+			if r < 0:
+				plant_cards.append(PlantCard.new(i))
+				break
 
 	for i in range(60):
 		if i < 10:
